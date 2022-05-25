37a4a9b49ddd1b53a435adceb05518834744d69a
Michael Chan <mckchan13@gmail.com>
2022-05-25 06:17:03 -0700

    installed concurrently and rewrote scripts so that all services can be started with one command

--- gateway/index.ts
+++ gateway/index.ts
@@ -22,5 +22,5 @@ app.use('/user', proxy('http://localhost:3001'));
 // });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Gateway listening on port ${PORT}`);
 });
