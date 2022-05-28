a858280140828beef8e0eefef9bae2451da9789c
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-05-28 09:07:46 -0700

    updates to userServer

--- services/userService/userServer.ts
+++ services/userService/userServer.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+const userdb = require('./userdb.json');
+const cors = require('cors');
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client(userdb.GOOGLE_CLIENT_ID);
 
 const app = express();
 const PORT = process.env.USERPORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', (req, res, next) => {
   res.send('From user microservices');
