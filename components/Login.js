cd13a2393a2e37ae473cb890abf3d43b79483d70
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-05-28 06:55:19 -0700

    ts-node error but google login into login component

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
 export default function Login() {
   return (
     <div>
       <h1>Login please</h1>
+
+      <GoogleLogin
+        clientId='214015961785-plma5hibritagflmrtnh58l76j58bi6f.apps.googleusercontent.com'
+        buttonText='Login'
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
     </div>
   );
 }
