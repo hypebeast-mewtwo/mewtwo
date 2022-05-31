8842375c883f66b02465353cbcb4ee241c6f5a2d
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-05-31 09:27:10 -0700

    error in env file cannot turn on db

--- services/userService/db.model.ts
+++ services/userService/db.model.ts
@@ -1,21 +1,28 @@
-const { Pool } = require('pg');
+import { Pool, QueryResult } from 'pg';
+
+require('dotenv').config();
+
+type Callback<T> = (err: Error, result: QueryResult<T[]>) => T[];
+
+const port: number = 5432;
 
 const databaseParams = {
   username: process.env.USER_RDS_USERNAME,
   hostname: process.env.USER_RDS_HOSTNAME,
   dbname: process.env.USER_RDS_DBNAME,
   password: process.env.USER_RDS_PASSWORD,
-  port: process.env.USER_RDS_PORT,
+  port: port,
 };
 
 const pool = new Pool(databaseParams);
+
 pool.on('connect', () => {
   console.log('connected to userdb');
 });
 
 module.exports = {
   databaseParams,
-  query: (text: String, params: String, callback: String) => {
+  query: (text: string, params: any, callback: any) => {
     console.log('executed query', text);
     return pool.query(text, params, callback);
   },
